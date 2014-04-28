bb40f8c391ba4c26ffaf3b76279153deeeafcf52
Chao Pang <ChaoPang229@gmail.com>
2014-04-28 11:39:41 +0200

    store rdfs info in Java objects

--- src/main/java/org/datafairport/dcat/jena/Catalog.java
+++ src/main/java/org/datafairport/dcat/jena/Catalog.java
@@ -1,7 +1,92 @@
 package org.datafairport.dcat.jena;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hp.hpl.jena.graph.Node;
+
 /**
  * Created by mswertz on 27/04/14.
  */
-public class Catalog {
+public class Catalog
+{
+	private final Node subject;
+	private Node title;
+	private Node homePage;
+	private Node modified;
+	private final List<Dataset> dataSet = new ArrayList<Dataset>();
+
+	public Catalog(Node subject)
+	{
+		this.subject = subject;
+	}
+
+	public Node getTitle()
+	{
+		return title;
+	}
+
+	public String getTitleValue()
+	{
+		return convertToValue(title);
+	}
+
+	public void setTitle(Node title)
+	{
+		this.title = title;
+	}
+
+	public Node getHomePage()
+	{
+		return homePage;
+	}
+
+	public String getHomePageValue()
+	{
+		return convertToValue(homePage);
+	}
+
+	public void setHomePage(Node homePage)
+	{
+		this.homePage = homePage;
+	}
+
+	public Node getModified()
+	{
+		return modified;
+	}
+
+	public String getModifiedValue()
+	{
+		return convertToValue(modified);
+	}
+
+	public void setModified(Node modified)
+	{
+		this.modified = modified;
+	}
+
+	public Node getSubject()
+	{
+		return subject;
+	}
+
+	public List<Dataset> getDataSet()
+	{
+		return dataSet;
+	}
+
+	private String convertToValue(Node node)
+	{
+		String value = null;
+		if (node.isLiteral())
+		{
+			value = node.isLiteral() ? node.getLiteralValue().toString() : node.getLocalName();
+		}
+		else
+		{
+			value = node.getLocalName().isEmpty() ? node.getNameSpace() : node.getLocalName();
+		}
+		return value;
+	}
 }
